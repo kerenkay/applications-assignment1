7837bfa02edbabb298dd9ce80df7fa00ddb36249
keren kayrich <keren.kay.2001@gmail.com>
2025-12-18 13:03:16 +0200

    add option to play again

--- app/src/main/java/com/example/applications_assignment1/MainActivity.kt
+++ app/src/main/java/com/example/applications_assignment1/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.applications_assignment1
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -132,18 +134,22 @@ class MainActivity : AppCompatActivity() {
         if (lives == 0) {
             gameOver()
         } else {
-            Toast.makeText(this, "crash!!", Toast.LENGTH_SHORT).show()
+            if (lives == 2)
+                Toast.makeText(this, "oh noo!!", Toast.LENGTH_SHORT).show()
+            if (lives == 1)
+                Toast.makeText(this, "Last chance!", Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun gameOver(){
+    fun gameOver() {
         Toast.makeText(this, "Game Over", Toast.LENGTH_SHORT).show()
         clearMonsters()
         handler.removeCallbacks(gameLoop)
         unableBtn()
+        showGameOverDialog()
     }
 
-    fun unableBtn(){
+    fun unableBtn() {
         binding.btnLeft.isEnabled = false
         binding.btnRight.isEnabled = false
     }
@@ -173,168 +179,28 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun showGameOverDialog() {
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle("Game Over!")
+        builder.setMessage("Do you want to play again or exit?")
+
+        builder.setPositiveButton("New Game") { dialog, which ->
+            startNewGame()
+        }
+
+        builder.setNegativeButton("Exit") { dialog, which ->
+            finish()
+        }
+
+        builder.setCancelable(false)
+        builder.create().show()
+    }
+
+    private fun startNewGame() {
+        val intent = Intent(this, MainActivity::class.java)
+        finish()
+        startActivity(intent)
+    }
 }
-
-
-
-
-//class MainActivity : AppCompatActivity() {
-//
-//    private lateinit var binding: ActivityMainBinding
-//    private lateinit var MonstersMap: Array<Array<ImageView>>
-//    private lateinit var childMap: Array<ImageView>
-//    private lateinit var gameManager: GameManager
-//    private var lives: Int = 3
-//    val DELAY: Long = 600
-//    val NUM_OF_ROADS =3
-//    val NUM_OF_ROWS=5
-//    var childPosition = 1
-//    var addMonster=true
-//    private val activeMonsters = mutableListOf<Monster>()
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//
-//        MonstersMap = arrayOf(
-//            arrayOf(binding.imgMonster00, binding.imgMonster01, binding.imgMonster02),
-//            arrayOf(binding.imgMonster10, binding.imgMonster11, binding.imgMonster12),
-//            arrayOf(binding.imgMonster20, binding.imgMonster21, binding.imgMonster22),
-//            arrayOf(binding.imgMonster30, binding.imgMonster31, binding.imgMonster32),
-//            arrayOf(binding.imgMonster40, binding.imgMonster41, binding.imgMonster42),
-//        )
-//
-//        childMap =arrayOf(binding.imgChild00, binding.imgChild01, binding.imgChild02)
-//
-//        updateChildPosition()
-//
-//        binding.btnLeft.setOnClickListener {
-//            goLeft()
-//        }
-//
-//        binding.btnRight.setOnClickListener {
-//            goRight()
-//        }
-//
-////        gameManager = GameManager(numRows = NUM_OF_ROWS, numCols = NUM_OF_ROADS)
-//        handler.post(gameLoop)
-//
-//
-//    }
-//
-//    fun goLeft() {
-//        if (childPosition > 0) {
-//            childPosition--
-//            updateChildPosition()
-//        }
-//    }
-//
-//    fun goRight() {
-//        if (childPosition < NUM_OF_ROADS -1) {
-//            childPosition++
-//            updateChildPosition()
-//        }
-//    }
-//
-//    fun updateChildPosition() {
-//        for ( child in childMap){
-//            child.visibility = View.INVISIBLE
-//        }
-//        childMap[childPosition].visibility = View.VISIBLE
-//    }
-//
-//    fun clearMonsters() {
-//        for (row in 0 until NUM_OF_ROWS) {
-//            for (col in 0 until NUM_OF_ROADS) {
-//                MonstersMap[row][col].visibility = View.INVISIBLE
-//            }
-//        }
-//    }
-//
-//    fun moveTheMonsters() {
-//        //move down the Monsters
-//        val it = activeMonsters.iterator()
-//        while (it.hasNext()) {
-//            val monster = it.next()
-//            monster.row += 1
-//            if (monster.row >= NUM_OF_ROWS) {
-//                it.remove()
-//            }
-//        }
-//
-//        // add new monster
-//        if(addMonster) {
-//            val newCol = (0 until NUM_OF_ROADS).random()
-////        if(newCol >= 0) {
-//            activeMonsters.add(Monster(row = 0, col = newCol))
-//            addMonster = false
-////        }
-//        }else{
-//            addMonster =true
-//        }
-//
-//        updateMonsters()
-//    }
-//
-//    fun updateMonsters() {
-//        clearMonsters()
-//        for (monster in activeMonsters) {
-//            MonstersMap[monster.row][monster.col].visibility = View.VISIBLE
-//        }
-//    }
-//
-//    fun checkCrash(){
-//        for (monster in activeMonsters){
-//            if(monster.row == NUM_OF_ROWS-1 && monster.col == childPosition){
-//                lives--
-//                crash()
-//            }
-//        }
-//    }
-//
-//    private fun crash() {
-//        binding.heart1.visibility = if (lives >= 1) View.VISIBLE else View.INVISIBLE
-//        binding.heart2.visibility = if (lives >= 2) View.VISIBLE else View.INVISIBLE
-//        binding.heart3.visibility = if (lives >= 3) View.VISIBLE else View.INVISIBLE
-//        vibrate()
-//        if(lives == 0){
-//            Toast.makeText(this, "Game Over", Toast.LENGTH_SHORT).show()
-//            clearMonsters()
-//        }else{
-//            Toast.makeText(this, "crash!!", Toast.LENGTH_SHORT).show()
-//        }
-//    }
-//
-//    private fun vibrate() {
-//        val v = getSystemService(VIBRATOR_SERVICE) as Vibrator
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
-//        } else {
-//            v.vibrate(500)
-//        }
-//    }
-//
-//
-//    val handler: Handler = Handler(Looper.getMainLooper())
-//    val gameLoop = object : Runnable {
-//        override fun run() {
-//            moveTheMonsters()
-//            checkCrash()
-//            if(lives>0)
-//                handler.postDelayed(this, DELAY)
-//        }
-//    }
-//
-//
-//
-//}
